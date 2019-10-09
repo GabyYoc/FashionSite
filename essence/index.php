55ed825d0fb7d21bc11867ff681cfc8a01d40889
GabyYoc <gabyyoc11@gmail.com>
2019-10-09 08:27:25 -0600

    Edición del heder.Eliminar páginas sobrantes.

--- essence/index.php
+++
@@ -1,9 +0,0 @@
-<?php include "header.php"; ?>
-	<?php
-        echo "hola :3 me quiero ir a mi casita!!";
-        $array = [1,2,3];
-        foreach ($array as $i) {
-            echo "<p>($i)</p>";
-        }
-    ?>
-<?php require "footer.php"; ?>
