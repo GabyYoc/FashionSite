d140ed75ad34ffb2a8c6b648923a58bea7986ba3
GabyYoc <gabyyoc11@gmail.com>
2019-10-09 07:31:48 -0600

    Template

---
+++ essence/index.php
@@ -0,0 +1,9 @@
+<?php include "header.php"; ?>
+	<?php
+        echo "hola :3 me quiero ir a mi casita!!";
+        $array = [1,2,3];
+        foreach ($array as $i) {
+            echo "<p>($i)</p>";
+        }
+    ?>
+<?php require "footer.php"; ?>
